b131409cb8ef8a2885f4f08005d14a0948624d4c
srijansrivastava1234 <srivastavasrijan258@gmail.com>
2026-08-12 21:36:33 +0530

    feat: initial spring boot project skeleton setup with health controller

---
+++ src/test/java/com/ipl/auction/IplAuctionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipl.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplAuctionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
