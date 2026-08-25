81ae65fe5493ba609716a299799d752707d32c80
srijansrivastava1234 <srivastavasrijan258@gmail.com>
2026-08-25 22:50:51 +0530

    feat: restore maven wrapper, fix duplicate EnableJpaAuditing, fix H2 year reserved word, and fix lazy load proxy in tests

--- src/main/java/com/ipl/auction/IplAuctionApplication.java
+++ src/main/java/com/ipl/auction/IplAuctionApplication.java
@@ -2,15 +2,13 @@ package com.ipl.auction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableTransactionManagement
 public class IplAuctionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IplAuctionApplication.java, args);
+        SpringApplication.run(IplAuctionApplication.class, args);
     }
 }
